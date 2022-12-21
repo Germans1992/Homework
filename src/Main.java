acb9f32e28d3aea51a8c7f1b1c1e42e3ce99c95a
Germans <germanstitov13@gmail.com>
2022-12-22 00:36:28 +0200

    MyHomework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+Person person = new Person(17, 90,"German", 192);
+Person secondPerson = new Person("German",30,181,90);
+        System.out.println(person);
+        System.out.println(secondPerson);
+        System.out.println("--------------");
+
+        Person vladimir = new Person(38,10,100);
+        System.out.println(vladimir);
+        person.go();
+        vladimir.order();
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    String name;
+    int age;
+    int height;
+    int weight;
+int size;
+int quantity;
+int price;
+
+    public Person(String name, int age, int height, int weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Person(int a, int w, String n, int h){
+        {
+            this.age = a;
+            this.weight = w;
+            this.name = n;
+            this.height = h;
+        }
+    }
+    public Person(int size, int quantity, int price){
+        this.size = size;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    //Попробовал такой вариант(при покупке больше 10 пар обуви цена уменьшается на 10 процентов);
+    public void order(){
+        if(quantity > 10){
+            System.out.println("You ordered more than 10 pieces your price is " + (price - (price * 10 / 100)) + "$");
+        }else{
+            System.out.println("You ordered 10 or less then 10 pieces, your price is " + price + "$");
+        }
+    }
+    public void go(){
+        if(age < 18 || age > 70){
+            System.out.println("I can't work i need to have a rest");
+        }else{
+            System.out.println("I need to work");
+        }
+    }
+
+    // дополнительное задание не понял ( именно -  Добавить поле "money" ) какое имелось ввиду поле, поздно делал не стал писать вам!
+    //При написании Private — доступ открыт только самому классу и друзьям данного класса.
+
 }
